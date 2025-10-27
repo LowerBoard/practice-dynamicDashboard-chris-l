3e1b30fc72d2b613ec25be2769987a3ff8955053
cmlorbert <cmlorbert@gmail.com>
2025-10-27 00:57:30 -0500

    done

--- Dynamic-Dashboard/src/components/DynamicDashboard.jsx
+++ Dynamic-Dashboard/src/components/DynamicDashboard.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
 const userName = 'Frodo';
-const isPremium = true
+const isPremium = true;
+const tasks = [
+    {
+        taskName: 'taking out the trash',
+        completed: true
+    },
+    {
+        taskName: 'doing dishes',
+        completed: true
+    },
+    {
+        taskName: 'raking leaves',
+        completed: false
+    }
+];
 
 const DynamicDashboard = () => {
     return(
         <div>
             <h1 className='greeting'>Hello {userName}!</h1>
+            {isPremium ? <h2>Thank you for being a Premium Member!</h2> : <h2>Upgrade to Premium to enjoy Exclusive Features!</h2>}
+            {tasks.map((task) => task.completed ? <p>{task.taskName} - ✅</p> : <p>{task.taskName} - ❌</p>)}
         </div>
     );
 };
